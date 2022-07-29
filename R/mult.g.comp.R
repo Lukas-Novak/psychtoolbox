4c25c6801c2bc0926fbc6ab5567f8b41bd53c491
Lukáš Novák <lukasjirinovak@gmail.com>
2022-07-29 07:53:26 +0000

    fix of quote

--- R/mult.g.comp.R
+++ R/mult.g.comp.R
@@ -90,7 +90,7 @@
 #' @export
 #......................................................
 
-mult.g.comp = function(df,outcome.var,groups) {
+mult.g.comp = function(df,outcome.var,groups,desc.stat = NULL) {
   {
     desc.tab = function(groups, outcome.var, df) {
       factors.dat = df %>% select(where(is.factor)) %>% names()
@@ -106,31 +106,31 @@ mult.g.comp = function(df,outcome.var,groups) {
         mutate(percent = n / sum(n)*100) %>%
         ungroup()
     }
-
-
+    
+    
     b = desc.tab(groups, outcome.var, df) %>%
       mutate(value = str_replace(value, "NA NA", "Missing"))
-
+    
     if(sum(b$n <= 1) >= 1){
       stop("There is less than 1 observation in some factor level, please remove it or merge to another factor level")
     }
-
+    
     # filtering of NAs in data - this is causing problems when merging
     dat =df %>%
       select(ends_with(vctrs::vec_c(groups,outcome.var)))
     #filter(!if_any(c(paste(groups,sep = "|")), is.na))
-
+    
     nam.ex = b %>%
       select(ends_with(c("_mean","_sd"))) %>%
       names()
-
+    
     outcome.var = b %>%
       select(ends_with(c("_mean","_sd"))) %>%
       names() %>%
       str_replace_all(., "_mean", "") %>%
       str_replace_all(., "_sd", "") %>%
       unique()
-
+    
     b = b %>%
       ungroup() %>%
       mutate(across(paste(nam.ex), ~as.numeric(.))) %>%
@@ -150,16 +150,16 @@ mult.g.comp = function(df,outcome.var,groups) {
       ungroup() %>%
       select(!c(id,dups))
   }
-
+  
   # odstaranit duplikáty v key prostřednictím funkce duplicate
-
+  
   # selecting all groups and outome variables
   factors.dat = dat %>% select(where(is.factor)) %>% names()
   output.var = dat %>% select(where(is.numeric)) %>% names()
-
+  
   dat.factors = dat %>%
     mutate(across(paste0(factors.dat), ~paste(as.numeric(.), .)))
-
+  
   ## comparison for 2 groups
   ##.....................................................................................
   # if(dat %>% select_if(~ nlevels(.) == 2) %>% length() >= 1) {
@@ -169,7 +169,7 @@ mult.g.comp = function(df,outcome.var,groups) {
     tidyr::pivot_longer(cols = c(where(is.factor)),
                         names_to = "key",
                         values_to = "value")
-
+  
   #   # homogeneity testing of the 2 groups
   hom.t.2groups = dat2.two.groups %>%
     group_by(key) %>%
@@ -177,7 +177,7 @@ mult.g.comp = function(df,outcome.var,groups) {
     pivot_longer(paste0(output.var),
                  names_to = "names_continous_var",
                  values_to = "p_val_homo")
-
+  
   # normality testing
   norm.test.2groups = dat2.two.groups %>%
     group_by(key) %>%
@@ -185,7 +185,7 @@ mult.g.comp = function(df,outcome.var,groups) {
     pivot_longer(paste0(output.var),
                  names_to = "names_continous_var",
                  values_to = "p_val_shapiro")
-
+  
   # non-parametric testing and merging homogeneity and normality
   gen.tab.krus.2groups = dat2.two.groups %>%
     group_by(key) %>%
@@ -204,12 +204,12 @@ mult.g.comp = function(df,outcome.var,groups) {
     filter(stat.p.value < 0.05) %>%
     mutate(homo_non_normal = p_val_homo > 0.05 & p_val_shapiro.p.value < 0.05,
            non_homo_normal = p_val_homo < 0.05)
-
+  
   # there starts sequence
   d.2groups =  dat2.two.groups %>%
     group_by(key) %>%
     group_by(key)
-
+  
   # Non-normality
   if (any(gen.tab.krus.2groups$homo_non_normal == TRUE)) {
     non.norm.var.wilc=filter(.data = gen.tab.krus.2groups, homo_non_normal == TRUE)
@@ -247,11 +247,11 @@ mult.g.comp = function(df,outcome.var,groups) {
       # there is need to filter results which are not referring to proper results of the Dunn test
       filter(str_detect(merged_cols,
                         paste0(non.norm.var.wilc$key,",",non.norm.var.wilc$names_continous_var,collapse = "|"))) %>%  
-       distinct(.statistic, .p, .keep_all = T) %>%  # filtering duplicated values across the two columns
+      distinct(.statistic, .p, .keep_all = T) %>%  # filtering duplicated values across the two columns
       mutate(results_agregated = paste0(str_extract(.group1, "^.{1}"), " vs ",
                                         str_extract(.group2, "^.{1}"),", ",
                                         "W = ", .statistic,", ", .p))
-
+    
     # Creating aggregated results to join into descriptive table
     aggregated.results.wilcox = wilcox.test.results %>%
       select(starts_with(c("key","names_cont","results_agre","merged_cols"))) %>%
@@ -263,7 +263,7 @@ mult.g.comp = function(df,outcome.var,groups) {
       mutate(names_continous_var = paste0(names_continous_var," Group difference")) %>%
       pivot_wider(names_from = names_continous_var, values_from = `Group comparison`)
   }
-
+  
   # Homoscedasticity
   if (any(gen.tab.krus.2groups$non_homo_normal == TRUE)) {
     non.homo.var.welch=filter(.data = gen.tab.krus.2groups, non_homo_normal == TRUE)
@@ -306,11 +306,11 @@ mult.g.comp = function(df,outcome.var,groups) {
       # there is need to filter results which are not referring to proper results of the Dunn test
       filter(str_detect(merged_cols,
                         paste0(non.homo.var.welch$key,",",non.homo.var.welch$names_continous_var,collapse = "|"))) %>% 
-     distinct(.statistic, .p, .keep_all = T) %>%  # filtering duplicated values across the two columns
+      distinct(.statistic, .p, .keep_all = T) %>%  # filtering duplicated values across the two columns
       mutate(results_agregated = paste0(str_extract(.group1, "^.{1}"), " vs ",
                                         str_extract(.group2, "^.{1}"),", ",
                                         "t(",.df,")"," = ",.statistic,", ", .p))
-
+    
     # Creating aggregated results to join into descriptive table
     aggregated.results.welch = Welch.test.results %>%
       select(starts_with(c("key","names_cont","results_agre","merged_cols"))) %>%
@@ -337,7 +337,7 @@ mult.g.comp = function(df,outcome.var,groups) {
       pivot_longer(paste0(output.var),
                    names_to = "names_continous_var",
                    values_to = "p_val_homo")
-
+    
     # normality testing
     norm.test = dat2 %>%
       group_by(key) %>%
@@ -345,7 +345,7 @@ mult.g.comp = function(df,outcome.var,groups) {
       pivot_longer(paste0(output.var),
                    names_to = "names_continous_var",
                    values_to = "p_val_shapiro")
-
+    
     # non-parametric testing and merging homogeneity and normality
     gen.tab.krus = dat2 %>%
       group_by(key) %>%
@@ -364,12 +364,12 @@ mult.g.comp = function(df,outcome.var,groups) {
       filter(stat.p.value < 0.05) %>% # there is need to turn on this after testing !!!!
       mutate(homo_non_normal = p_val_homo > 0.05 & p_val_shapiro.p.value < 0.05,
              non_homo_normal = p_val_homo < 0.05)
-
+    
     # there starts sequence
     d =  dat2 %>%
       group_by(key) %>%
       group_by(key)
-
+    
     # Non-normality
     if (any(gen.tab.krus$homo_non_normal == TRUE)) {
       # this is for the further development with normal and homoscedastics data
@@ -423,7 +423,7 @@ mult.g.comp = function(df,outcome.var,groups) {
         mutate(results_agregated = paste0(str_extract(.group1, "^.{1}"), " vs ",
                                           str_extract(.group2, "^.{1}"),", ",
                                           "z = ", .statistic,", ", .p.adj))
-
+      
       # Creating aggregated results to join into descriptive table
       aggregated.results.dunn = dunn.test.results %>%
         select(starts_with(c("key","names_cont","results_agre","merged_cols"))) %>%
@@ -435,14 +435,14 @@ mult.g.comp = function(df,outcome.var,groups) {
         mutate(names_continous_var = paste0(names_continous_var," Group difference")) %>%
         pivot_wider(names_from = names_continous_var, values_from = `Group comparison`)
     }
-
+    
     # estimation of the effect size from R package - Rcompanion
     #..................................................
     # Matrix = outer(A, B, FUN = "-") # A = X-variable, B = Y-variable
     # Diff = ifelse(Matrix == 0, 0.5, Matrix > 0)
     # VDA = signif(mean(Diff), digits = 2)
     #..................................................
-
+    
     # Homoscedasticity
     if (any(gen.tab.krus$non_homo_normal == TRUE)) {
       non.homo.var=filter(.data = gen.tab.krus, non_homo_normal == TRUE) # this makes troubles | non_homo_normal == TRUE
@@ -488,14 +488,14 @@ mult.g.comp = function(df,outcome.var,groups) {
                across(ends_with(c(".statistic",".df")), ~round(., 2))) %>%
         # there is need to filter results which are not referring to proper results of the Dunn test
         filter(str_detect(merged_cols,
-                          paste0(non.homo.var$key,",",non.homo.var$names_continous_var,collapse = "|")))
-        distinct(.estimate, .statistic, .keep_all = T) %>%  # filtering duplicated values across the two columns         
+                          paste0(non.homo.var$key,",",non.homo.var$names_continous_var,collapse = "|"))) %>% 
+      distinct(".estimate", ".statistic", .keep_all = T) %>%  # filtering duplicated values across the two columns         
         mutate(.p.adj = format_p(.p.adj)) %>%
         mutate(.estimate = round(.estimate,digits = 2)) %>%
         mutate(results_agregated = paste0(str_extract(.group1, "^.{1}"), " vs ",
                                           str_extract(.group2, "^.{1}"),", ",
                                           "t(",.df,")"," = ",.estimate,", ", .p.adj))
-
+      
       # Creating aggregated results to join into descriptive table
       aggregated.results.games.howell = games.howell.test.results %>%
         select(starts_with(c("key","names_cont","results_agre","merged_cols"))) %>%
@@ -507,12 +507,12 @@ mult.g.comp = function(df,outcome.var,groups) {
         mutate(names_continous_var = paste0(names_continous_var," Group difference")) %>%
         pivot_wider(names_from = names_continous_var, values_from = `Group comparison`)
     }
-
+    
     # https://stackoverflow.com/a/45515491/14041287
     coalesce_by_column <- function(df) {
       return(dplyr::coalesce(!!! as.list(df)))
     }
-
+    
     if(exists("aggregated.results.games.howell") & exists("aggregated.results.dunn")) {
       psd = aggregated.results.dunn %>%
         full_join(aggregated.results.games.howell) %>%
@@ -528,7 +528,7 @@ mult.g.comp = function(df,outcome.var,groups) {
     } else if (!exists("aggregated.results.games.howell") & !exists("aggregated.results.dunn")) {
       psd = b
     }
-
+    
     psd = psd %>%
       mutate(across(ends_with("Group difference"), ~replace(., duplicated(.), ""))) %>%
       mutate_all(~replace(., is.na(.), "")) %>%
@@ -536,16 +536,16 @@ mult.g.comp = function(df,outcome.var,groups) {
       mutate(n = as.numeric(n)) %>%
       mutate(across(ends_with(c("_mean","_sd","percent")), ~as.numeric(.)))
   }
-
+  
   two.level.factors = dat %>%
     select_if(~ nlevels(.) == 2) %>% names()
-
+  
   if(exists("aggregated.results.wilcox")) {
     comb.wilcox.pre = aggregated.results.wilcox %>%
       full_join(b) %>%
       filter(str_detect(key, paste0(two.level.factors,collapse = "|")))
   }
-
+  
   if(exists("comb.wilcox.pre")) {
     comb.wilcox.pre.fin = comb.wilcox.pre %>%
       full_join(psd) %>%
@@ -560,7 +560,7 @@ mult.g.comp = function(df,outcome.var,groups) {
       select(!dups) %>% 
       mutate_all(~(replace(., is.na(.), "")))
   }
-
+  
   if(exists("aggregated.results.welch")) {
     comb.welch.pre = aggregated.results.welch %>%
       full_join(b) %>%
@@ -597,14 +597,14 @@ mult.g.comp = function(df,outcome.var,groups) {
       select(!dups) %>% 
       mutate_all(~(replace(., is.na(.), "")))
   }
-#.......................................
+  #.......................................
   if(exists("comb.wilcox.pre.fin") & exists("comb.welch.fin")) {
     comb.wilcox.pre.fin = comb.wilcox.pre.fin %>%
       full_join(comb.welch.fin) %>% 
       mutate(across(contains("Group difference"), ~ifelse(is.na(.), "", .)))
-
+    
     sort.names = comb.wilcox.pre.fin %>% select(ends_with(c("key","value","n","percent","Group difference"))) %>% names()
-
+    
     comb.wilcox.pre.fin = comb.wilcox.pre.fin %>%
       relocate(all_of(sort.names)) %>% 
       return(comb.welch.fin)
@@ -629,14 +629,14 @@ mult.g.comp = function(df,outcome.var,groups) {
       sort.names = comb.wilcox.pre.fin %>% select(ends_with(c("key","value","n","percent","Group difference"))) %>% names()
       wilcox.to.print =  comb.wilcox.pre.fin %>%
         relocate(all_of(sort.names)) 
-        return(wilcox.to.print) 
+      return(wilcox.to.print) 
     }
     
     if(exists("solo.welsh.fin")) {
-        sort.names = solo.welsh.fin %>% select(ends_with(c("key","value","n","percent","Group difference"))) %>% names()
-        solo.welsh.to.print =  solo.welsh.fin %>%
-          relocate(all_of(sort.names)) 
-          return(solo.welsh.to.print)
+      sort.names = solo.welsh.fin %>% select(ends_with(c("key","value","n","percent","Group difference"))) %>% names()
+      solo.welsh.to.print =  solo.welsh.fin %>%
+        relocate(all_of(sort.names)) 
+      return(solo.welsh.to.print)
     }
     # if(!exists("comb.welch.fin") & !exists("comb.wilcox.pre.fin")) {
     #   sort.names = comb.welch.fin %>% select(ends_with(c("key","value","n","percent","Group difference"))) %>% names()
