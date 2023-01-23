3b9e431c56d6ef7c54abeab54a1e170a1751b372
lukas.novak <lukasjirinovak@gmail.com>
2023-01-23 10:32:48 +0100

    after adding RMSEA index evaluating mi

--- R/mitab.R
+++ R/mitab.R
@@ -10,10 +10,11 @@
 #' @param std.lv logical, if TRUE than standardized loadings are stored in temporal output
 #' @param meanstructure logical, if TRUE than model with meanstructure is estimated
 #' @param group name of grouping variable
-#' @param yes_no_results logical, if TRUE than lasy output indicating difference between models is added, currently working only based on CFI
+#' @param yes_no_results logical, if TRUE than lasy output indicating difference between models is added, currently working only based on CFI and RMSEA
 #' @param estimator name of estimator to be used during fitting procedure
 #' @param robust logical, if TRUE, than robust results are printed, working only with estimators  providing robust results (e.g. MLR or DWLS)
 #' @param cfi.difference logical, if TRUE, delta of the CFI is printed in output
+#' @param rmsea.difference logical, if TRUE, delta of the RMSEA is printed in output
 #'
 #'
 #' @return data frame
@@ -67,7 +68,7 @@
 #' print(res.tab.mi)
 #' @export
 #......................................................
-mitab = function(group1_nam, group2_nam, ordered, model, data, std.lv, meanstructure, group, yes_no_results, estimator, robust = FALSE, cfi.difference = FALSE) {
+mitab = function(group1_nam, group2_nam, ordered, model, data, std.lv, meanstructure, group, yes_no_results, estimator, robust = FALSE, cfi.difference = FALSE, rmsea.difference = FALSE) {
   if(!is.factor(data[[group]])) stop("The 'group' argument has to be type factor" ) # group has to be factor in order to correctly name g.1 and g.2
   if(!is.data.frame(data)) stop('data must be a data frame')
 
@@ -197,10 +198,11 @@ mitab = function(group1_nam, group2_nam, ordered, model, data, std.lv, meanstruc
                                                           "rmsea.ci.upper.scaled", 'srmr')), digits = 3))
 
     # mult.group.m$eqMI.stat # checking chi-square
-
     tab.fit = tab.fit %>%
       as_tibble() %>%
-      mutate(rmsea = paste0(rmsea, " ","(",rmsea.ci.lower,"-",rmsea.ci.upper,")")) %>%
+      mutate(
+        rmsea2 = rmsea,
+        rmsea = paste0(rmsea, " ","(",rmsea.ci.lower,"-",rmsea.ci.upper,")")) %>%
       rename(
         "RMSEA (90% CI)" = "rmsea") %>%
       select(!starts_with(c("rmsea.ci.","rmsea.ci.lower.scaled","rmsea.ci.upper.scaled")))
@@ -234,9 +236,37 @@ mitab = function(group1_nam, group2_nam, ordered, model, data, std.lv, meanstruc
   }
   tab.fit = tab.fit %>%
     cfi.dif()
+
+  rmsea.dif = function(x) {
+    if(!is.data.frame(x)) stop('x must be a data frame')
+    if(length(x) < 1) stop('must be higher length than 1')
+    if(!sum(str_detect(names(x),"rmsea2"))) stop("There must be a column with rmsea2 values")
+    x = x %>%
+      tibble("RMSEA.dif" = c(NA, round(diff(as.numeric(rmsea2)),digits = 3))) %>%
+      relocate("RMSEA.dif", .after = `RMSEA (90% CI)`)
+    if(yes_no_results == TRUE) {
+      x = x %>% mutate("mod.dif" = ifelse(RMSEA.dif > 0.015, "Yes", "No"))
+    }
+    if(any(names(x) == 'mod.dif')) {
+      x = x %>% rename("Model difference - RMSEA" = "mod.dif")
+    }
+    x = x %>%
+      rename("RMSEA difference" = "RMSEA.dif")
+    if(rmsea.difference == FALSE)
+      x = x %>%
+      select(!"RMSEA difference")
+    print(x)
+    if(rmsea.difference == TRUE)
+      x = x %>%
+      rename("delta RMSEA" = "RMSEA difference")
+    print(x)
+  }
+  tab.fit = tab.fit %>%
+    rmsea.dif() %>%
+    select(-rmsea2)
 }
 
-# The famous Holzinger and Swineford (1939) example
+## The famous Holzinger and Swineford (1939) example
 # HS.model <- ' visual  =~ x1 + x2 + x3
 #               textual =~ x4 + x5 + x6
 #               speed   =~ x7 + x8 + x9 '
@@ -245,11 +275,12 @@ mitab = function(group1_nam, group2_nam, ordered, model, data, std.lv, meanstruc
 #                 group2_nam = "Pasteur",
 #                 ordered = FALSE,
 #                 model = HS.model,
-#                 data = HolzingerSwineford1939,
+#                 data = lavaan::HolzingerSwineford1939,
 #                 std.lv = T,
 #                 meanstructure = TRUE,
 #                 group = "school",
 #                 yes_no_results = TRUE,
 #                 estimator = "MLR",
 #                 robust = TRUE,
-#                 cfi.difference = TRUE)
+#                 cfi.difference = TRUE,
+#                 rmsea.difference = TRUE)
